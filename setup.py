206729f836cb26ddb95db6b945414d29f45ef620
Jonathan Raiman <jonathanraiman@gmail.com>
2014-10-27 14:49:49 -0400

    increase version and improve loading of cythin files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readfile(fname):
 
 setup(
     name='xml-cleaner',
-    version='1.0.6',
+    version='1.0.7',
     description='Python package for cleaning up xml and tokenizing text',
     long_description=readfile('README.md'),
     ext_modules=[],
--- xml_cleaner/__init__.py
+++ xml_cleaner/__init__.py
@@ -11,8 +11,8 @@ Usage
 import pyximport
 pyximport.install()
 
-from .wiki_markup_processing import to_raw_text, remove_brackets
-from .word_tokenizer import split_sentences, split_and_group_sentences, split_punct
+from wiki_markup_processing import to_raw_text, remove_brackets
+from word_tokenizer import split_sentences, split_and_group_sentences, split_punct
 
 __all__ = [
 	"to_raw_text",
