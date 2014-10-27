f12e207b33aee1910c445c70bcf7029ea55f84f7
Jonathan Raiman <jonathanraiman@gmail.com>
2014-10-27 14:52:14 -0400

    include pyx files in dist

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readfile(fname):
 
 setup(
     name='xml-cleaner',
-    version='1.0.7',
+    version='1.0.8',
     description='Python package for cleaning up xml and tokenizing text',
     long_description=readfile('README.md'),
     ext_modules=[],
