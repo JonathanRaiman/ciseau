5e1920e97728e835a8acda3ce1b6781a04bfbf15
Jonathan Raiman <jonathanraiman@gmail.com>
2014-10-27 14:12:39 -0400

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readfile(fname):
 
 setup(
     name='xml-cleaner',
-    version='1.0.5',
+    version='1.0.6',
     description='Python package for cleaning up xml and tokenizing text',
     long_description=readfile('README.md'),
     ext_modules=[],
