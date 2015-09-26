dd482a88866058d1b20365d5a63d3cc8555f443d
Jonathan Raiman <jonathanraiman@gmail.com>
2015-09-26 14:55:58 +0200

    update parser

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readfile(fname):
 
 setup(
     name='xml-cleaner',
-    version='1.0.20',
+    version='1.0.21',
     description='Python package for cleaning up xml and tokenizing text',
     long_description=readfile('README.md'),
     ext_modules=[],
